d32bfeeabb204fde49a587131f368320270d0bb4
JP Barela <barela@jpbarela.com>
2021-10-02 12:01:24 -0600

    Add NumericInput (#11)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // @flow strict-local
 export { Button } from "./Button";
+export { NumericInput } from "./Form";
 export { Row } from "./Layout";
 export { Page } from "./Page";
-export { ThemeProvider, ThemeContext } from "./Theme";
+export { ThemeProvider, ThemeContext, useTheme } from "./Theme";
 export { Body, Title } from "./Typography";
